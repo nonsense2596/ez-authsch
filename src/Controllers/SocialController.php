580fadbd0c8676bbb807879229b3fd788403ff8c
nonsense2596 <peter.szatmary@windowslive.com>
2021-03-20 18:50:32 +0100

    further refactorings

--- src/Controllers/SocialController.php
+++ src/Controllers/SocialController.php
@@ -20,8 +20,6 @@ use App\Http\Controllers\Controller as Controller;
 class SocialController extends Controller
 {
 
-
-    //////////////////////////////////////////////////////
     public function schonherzRedirect()
     {
         return Socialite::driver('authsch')->redirect();
@@ -29,19 +27,14 @@ class SocialController extends Controller
     public function loginWithSchonherz()
     {
         $user = Socialite::driver('authsch')->stateless()->user();
-        //ddd($user);
-        $authuser = null;
 
+        $authuser = null;
         if($dbuser = User::where('id',$user->id)->first()){
             $authuser = $dbuser;
-            $dbuser->delete();  // ez csak temp TODO
-            //ddd("lol");
         }
-        if(true){   // else // ez csak temp TODO
+        else{
 
-
-
-            // egyelore csak minden letezo adatot lementunk stringben
+            // ------====== BASIC USER INFORMATIONS ======------
             $authuser = User::create([
                 'id' => $user->id,
                 'displayName' => $user->displayName,
@@ -54,14 +47,11 @@ class SocialController extends Controller
                 'birthdate' => $user->birthdate,
             ]);
 
-
-
-
-
-
-            // ------====== linkedAccounts ======------  --> tarsitott accountok az sch acchoz
-            if(!is_null($user->linkedAccounts)){
-                foreach($user->linkedAccounts as $key => $value){
+            // ------====== LINKED ACCOUNTS ======------
+            if(!is_null($user->linkedAccounts))
+            {
+                foreach($user->linkedAccounts as $key => $value)
+                {
                     $linked_account = LinkedAccounts::create([
                         'user_id' => $user->id,
                         'account_type' => $key,
@@ -71,11 +61,11 @@ class SocialController extends Controller
                 }
             }
 
-
-
-            // ------====== eduPersonEntitlement ======------  --> pek kortagsagok
-            if(!is_null($user->eduPersonEntitlement)){
-                foreach($user->eduPersonEntitlement as $key => $value){
+            // ------====== CLUB MEMBERSHIPS ON PEK ======------
+            if(!is_null($user->eduPersonEntitlement))
+            {
+                foreach($user->eduPersonEntitlement as $key => $value)
+                {
                     //ddd($value["end"]);
                     $student_club_membership = StudentClubMemberships::create([
                         'user_id' => $user->id,
@@ -90,13 +80,12 @@ class SocialController extends Controller
                 }
             }
 
-
-
-            // ------====== niifEduPersonAttendedCourse ======------  --> felevben felvett kurzusok
+            // ------====== CURRENTLY (NOT ALWAYS UP TO DATE) LIST OF ATTENDED COURSES ======------
             if(!is_null($user->niifEduPersonAttendedCourse))
             {
                 $exploded = explode(";",$user->niifEduPersonAttendedCourse);
-                foreach($exploded as $value){
+                foreach($exploded as $value)
+                {
                     $attended_course = AttendedCourses::create([
                         'user_id' => $user->id,
                         'course' => $value
@@ -105,11 +94,11 @@ class SocialController extends Controller
                 }
             }
 
-
-
-            // ------====== entrants ======------  --> kollegiumi belepo stilus
-            if(!is_null($user->entrants)){
-                foreach($user->entrants as $value){
+            // ------====== DORM ENTRY CARD RANK ======------
+            if(!is_null($user->entrants))
+            {
+                foreach($user->entrants as $value)
+                {
                     $entrant = Entrants::create([
                         'user_id' => $user->id,
                         'group_id' => $value["groupId"],
@@ -117,43 +106,31 @@ class SocialController extends Controller
                         "entrant_type" => $value["entrantType"]
                     ]);
                     $entrant->save();
-                    //ddd($value);
                 }
             }
 
-
-
-            // ------====== admembership ======------  --> sch-s szolgáltatások listája
-            if(!is_null($user->admembership)){
-                foreach($user->admembership as $value){
+            // ------====== SCH ACTIVE DIRECTORY MEMBERSHIPS ======------
+            if(!is_null($user->admembership))
+            {
+                foreach($user->admembership as $value)
+                {
                     $admembership = AdMemberships::create([
                         'user_id' => $user->id,
                         'membership' => $value
                     ]);
                     $admembership->save();
-                    //ddd($value);
                 }
             }
-
-
-
-
-
 
 
             $authuser->save();
         }
 
-
         Auth::login($authuser);
 
-        //return redirect('/');
-        return redirect()->intended();  // igy redirectel jo helyre
+        return redirect()->intended();
     }
 
-
-
-    //////////////////////////////////////////////////////
 
     public function logOutOfSchonFuckingHerz()
     {
@@ -161,26 +138,8 @@ class SocialController extends Controller
         return redirect('/');
     }
 
-
-    public function multi_implode($array, $glue)
-    {
-        if(is_null($array)){
-            return null;
-        }
-        $ret = '';
-        foreach ($array as $item) {
-            if (is_array($item)) {
-                $ret .= self::multi_implode($item, $glue) . $glue;
-            } else {
-                $ret .= $item . $glue;
-            }
-        }
-        $ret = substr($ret, 0, 0-strlen($glue));
-        return $ret;
-    }
-
     public function get_unit_scope($bmeunitscope){
-        // ------====== bmeunitscope ======------  --> jogviszony, valaha, aktiv, vikes aktiv,...
+        // ------====== BME UNIT SCOPE (BME, VIK, ACTIVE, PASSIVE...) ======------
         $bmeunitscopes = self::multi_implode($bmeunitscope,";");
 
         $actualscope = null;
@@ -202,6 +161,23 @@ class SocialController extends Controller
             }
         }
         return $actualscope;
+    }
+
+    public function multi_implode($array, $glue)
+    {
+        if(is_null($array)){
+            return null;
+        }
+        $ret = '';
+        foreach ($array as $item) {
+            if (is_array($item)) {
+                $ret .= self::multi_implode($item, $glue) . $glue;
+            } else {
+                $ret .= $item . $glue;
+            }
+        }
+        $ret = substr($ret, 0, 0-strlen($glue));
+        return $ret;
     }
 
 }

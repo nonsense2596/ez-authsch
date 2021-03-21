2a1c7269c8e41476f9e4b95b9a49e83152e4f748
nonsense2596 <peter.szatmary@windowslive.com>
2021-03-21 01:34:51 +0100

    added stub for example controller displaying all user data

---
+++ src/Controllers/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Authsch;
+
+use App\Models\Authsch\AdMemberships;
+use App\Models\Authsch\User;
+use App\Models\Authsch\AttendedCourses;
+use App\Models\Authsch\LinkedAccounts;
+use App\Models\Authsch\StudentClubMemberships;
+use App\Models\Authsch\Entrants;
+use Illuminate\Http\Request;
+
+use App\Http\Controllers\Controller as Controller;
+
+class IndexController extends Controller
+{
+    public function index(){
+        return 'TODO implement user controller';
+    }
+}
